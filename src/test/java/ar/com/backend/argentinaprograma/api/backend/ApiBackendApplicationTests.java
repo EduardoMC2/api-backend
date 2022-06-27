5a8fe98d3568eebf61dfb76ad6fe9462ba4b9f99
Eduardo <eduundav@gmail.com>
2022-06-26 22:01:03 -0300

    nuevo intento

---
+++ src/test/java/ar/com/backend/argentinaprograma/api/backend/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.backend.argentinaprograma.api.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
